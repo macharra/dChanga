c38796c1c809ad7be77fca4d667dbf5b4358607f
Princemicah001 <micahprincemicah001@gmail>
2025-10-25 05:01:46 +0300

    border-border

--- app/page.tsx
+++ app/page.tsx
@@ -184,7 +184,6 @@ export default function Home() {
         </div>
       </section>
 
-
       {/* Footer */}
       <footer className="border-t border-border bg-card px-4 py-12 sm:px-6 lg:px-8">
         <div className="mx-auto max-w-7xl">
